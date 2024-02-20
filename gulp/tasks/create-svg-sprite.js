70f861b26f598faee2eb53aa0a95ceff41399bfe
Ulukbek Bakytbekov <ulukbakytbekov@gmail.com>
2024-02-20 09:22:57 +0600

    clearup

--- gulp/tasks/create-svg-sprite.js
+++ gulp/tasks/create-svg-sprite.js
@@ -13,9 +13,25 @@ const createSvgSprite = () => {
         mode: {
           symbol: {
             sprite: '../sprite.svg',
-
-            /** Создавать страницу с перечнем иконок */
             example: true,
+          },
+          selector: '%f',
+          transformData: function (data, config) {
+            data.svg.map(function (item) {
+              //change id attribute
+              item.data = item.data.replace(/id=\"([^\"]+)\"/gm, 'id="' + item.name + '-$1"');
+
+              //change id in fill attribute
+              item.data = item.data.replace(/fill=\"url\(\#([^\"]+)\)\"/gm, 'fill="url(#' + item.name + '-$1)"');
+
+              //change id in mask attribute
+              item.data = item.data.replace(/mask=\"url\(\#([^\"]+)\)\"/gm, 'mask="url(#' + item.name + '-$1)"');
+
+              //replace double id for the symbol tag
+              item.data = item.data.replace('id="' + item.name + '-' + item.name + '"', 'id="' + item.name + '"');
+              return item;
+            });
+            return data; // modify the data and return it
           },
         },
       })
